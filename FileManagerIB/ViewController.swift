ddce2efeadadb2a71cef64c227f6390f30180db3
Marymezh <maria.mezhova@yahoo.com>
2022-09-04 14:52:11 +0700

    Initial Commit

---
+++ FileManagerIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManagerIB
+//
+//  Created by Мария Межова on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
